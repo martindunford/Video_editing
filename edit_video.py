376d263c945a689d24499d9c226626165ea25034
Martin Dunford <martin.dunford@corehr.com>
2022-02-16 20:45:38 +0000

    Refactored so that a class Viktor handles all the video editing

--- edit_video.py
+++ edit_video.py
@@ -6,47 +6,87 @@ from setup_logger import *
 from sound_library import *
 import tempfile
 
+class Viktor:
+    ''' Viktor the video manipulator
+    '''
+    # --------------------
+    def __init__ (self,video_file):
+        self.video_file = video_file
+        self.complete_clip = VideoFileClip(self.video_file)
+        self.original_audio = self.complete_clip.audio
+
+    # --------------------
+    def find_peak_audio_amplitude(self):
+        tf = tempfile.NamedTemporaryFile(suffix='.wav')
+        self.info (f'Extracting audio track for Movie {self.video_file} into {tf.name}')
+        self.complete_clip.audio.write_audiofile(tf.name)
+
+        self.peak_audio,self.sample_rate = find_peak (tf.name)
+        self.info (f'Found audio peak for {self.video_file} at: {self.peak_audio} seconds')
+
+        return self.peak_audio, self.sample_rate
+
+    # --------------------
+    def add_audio_track (self, audio_file, insert_at, replace_original_audio=True):
+        if not self.sample_rate:
+            self.find_peak_audio_amplitude()
+
+        self.info (f'Adding additional audio (from {audio_file}) starting at: {insert_at} seconds')
+        audioclip1 = AudioFileClip(audio_file,fps=self.sample_rate)
+        # audioclip1.preview()
+
+        if replace_original_audio:
+            all_audio_clips = [
+                audioclip1.set_start(insert_at)
+            ]
+        else:
+            all_audio_clips = [
+                self.original_audio,
+                audioclip1.set_start(insert_at)
+            ]
+        # To add audio only way seems to be to assign a CompositeAudioClip
+        #  to clip.audio.
+        new_audioclip = CompositeAudioClip(all_audio_clips)
+        self.complete_clip.audio = new_audioclip
+
+        # Reduce the audio volume (volume x 1.8)
+        # clip = clip.volumex(1.2)
+
+    # --------------------
+    def add_text_overlay (self, mytext, insert_at, duration, font_size=70, mycolor='white'):
+        self.info (f'Adding text overlay at {insert_at} secs to be displayed for: {duration} secs ')
+        # Generate a text clip. You can customize the font, color, etc.
+        txt_clip = TextClip(mytext,fontsize=font_size,color=mycolor)
+
+        # Say that you want it to appear 10s at the center of the screen
+        txt_clip = txt_clip.set_pos('center').set_duration(duration)
+
+        # Overlay the text clip on the first video clip
+        self.all_video_clips = CompositeVideoClip([self.complete_clip, txt_clip])
+
+    # --------------------
+    def render_video_to (self, output_file):
+        self.info (f'Rendering to: {output_file}')
+        # Write the result to a file (many options available !)
+        self.all_video_clips.write_videofile(
+            output_file,
+            audio_codec='aac' # Should not be needed but there is bug.
+        )
+
+    # --------------------
+    def info(self, msg):
+        logger.info(f'{self.__class__.__name__}: {msg}')
+
 def main():
     video_file = 'htonk.mov'
-    add_audio = 'htonk.wav'
-    # Load video
-    clip = VideoFileClip(video_file)
 
-    tf = tempfile.NamedTemporaryFile(suffix='.wav')
-    logger.info (f'Extracting audio track for Movie {video_file} into {tf.name}')
-    clip.audio.write_audiofile(tf.name)
+    vik = Viktor(video_file)
+    peak_audio_point,sample_rate = vik.find_peak_audio_amplitude ()
 
-    peak_audio,sample_rate = find_peak (tf.name)
-    logger.info (f'Found audio peak at {peak_audio}')
+    vik.add_audio_track ('htonk.wav',peak_audio_point)
+    vik.add_text_overlay('Martin the Rolling Stone',4,10)
+    vik.render_video_to('coolio.mp4')
 
-    audioclip1 = AudioFileClip(add_audio,fps=sample_rate)
-    # Plays the audip
-    # audioclip1.preview()
-
-    # To add audio only way seems to be to assign a CompositeAudioClip
-    #  to clip.audio.
-    # So original track has to be explicitly part of that !! i.e clip.audio
-    # new_audioclip = CompositeAudioClip([audioclip1.set_start(peak_audio),clip.audio])
-    new_audioclip = CompositeAudioClip([audioclip1.set_start(peak_audio)])
-
-    # Reduce the audio volume (volume x 1.8)
-    # clip = clip.volumex(1.2)
-
-    # Generate a text clip. You can customize the font, color, etc.
-    txt_clip = TextClip("Rolling Stones",fontsize=70,color='white')
-
-    # Say that you want it to appear 10s at the center of the screen
-    txt_clip = txt_clip.set_pos('center').set_duration(10)
-
-    # Overlay the text clip on the first video clip
-    video = CompositeVideoClip([clip, txt_clip])
-    # Audio added here? but why not above like Text is!! 
-    video.audio = new_audioclip
-
-    # Write the result to a file (many options available !)
-    video.write_videofile("coolio.mp4",
-                          audio_codec='aac' # Should not be needed but there is bug.
-                          )
 
 if __name__ == '__main__':
     main()
